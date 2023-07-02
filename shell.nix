26caf24bd298ca3143602b2bd16d2458b63fa5d1
Dessix <dessix@dessix.net>
2023-07-01 17:31:03 -0700

    Minimized repro further through elimination of apparently unnecessary linkFarm indirection

--- shell.nix
+++ shell.nix
@@ -12,26 +12,12 @@ let
     filter = path: type:
       builtins.elem (baseNameOf path) [ "hello.txt" "world.txt" ];
   };
-  linkFarmed = pkgs.linkFarm "linkfarm-combined-dir" [
-    {
-      name = "hello.txt";
-      path = "${filteredPath}/hello.txt";
-    }
-    {
-      name = "world.txt";
-      path = "${filteredPath}/world.txt";
-    }
-    {
-      name = "space.txt";
-      path = "${pkgs.emptyFile}";
-    }
-  ];
   self = pkgs.stdenv.mkDerivation {
     name = "hello-world-lst-repro";
     version = "0.0.1";
     buildPhase = ''
-      ls "${linkFarmed}"
-      cat "${linkFarmed}/hello.txt" "${linkFarmed}/world.txt" > "$out/helloworld.txt"
+      ls "${filteredPath}"
+      cat "${filteredPath}/hello.txt" "${filteredPath}/world.txt" > "$out/helloworld.txt"
     '';
   };
 in self
